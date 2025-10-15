8163098944314dc32991b1557d4f48fe6a470533
Leo Cheng <lefan.cheng@outlook.com>
2025-10-16 00:01:48 +0800

    Update InsertCommand.java

--- src/main/java/seedu/duke/InsertCommand.java
+++ src/main/java/seedu/duke/InsertCommand.java
@@ -3,13 +3,20 @@ package seedu.duke;
 public class InsertCommand extends Command {
     private final Module module;
 
-    InsertCommand(String id, String moduleName, String day, String startTime, String endTime) {
-        this.module = new Module(id, moduleName, day, startTime, endTime);
+    InsertCommand(String id, String moduleName, String day, String startTime, String endTime, String sessionType) {
+        this.module = new Module(id, moduleName, day, startTime, endTime, sessionType);
     }
+
+    // Backward compatibility constructor
+    InsertCommand(String id, String moduleName, String day, String startTime, String endTime) {
+        this(id, moduleName, day, startTime, endTime, "lecture");
+    }
+
     @Override
     public void execute(UI ui, ModuleList modules) throws UniflowException {
-        // empty for now
         modules.addModule(module);
+        ui.showMessage("Got it! I've added this module:\n  " + module.toString() +
+                "\nNow you have " + modules.getSize() + " module(s) in the list.");
     }
 
     /**
