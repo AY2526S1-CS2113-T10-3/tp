19fd7283d112a4160c60048952e453445b01c23b
Leo Cheng <lefan.cheng@outlook.com>
2025-10-15 23:50:06 +0800

    Initialize FilterCommand.java

--- src/main/java/seedu/duke/FilterCommand.java
+++ src/main/java/seedu/duke/FilterCommand.java
@@ -1,5 +1,17 @@
 package seedu.duke;
 
 public class FilterCommand extends Command {
-    
+    private final String filterType;
+    private final String filterValue;
+
+    /**
+     * Creates a filter command with the specified type and value.
+     *
+     * @param filterType the type of filter (e.g., "type", "day", "id", "name", "hastutorial", "notutorial")
+     * @param filterValue the value to filter by (null for boolean filters like hastutorial)
+     */
+    public FilterCommand(String filterType, String filterValue) {
+        this.filterType = filterType.toLowerCase();
+        this.filterValue = filterValue;
+    }
 }
