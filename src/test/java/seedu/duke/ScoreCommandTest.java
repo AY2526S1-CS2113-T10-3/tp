6f91293c3480b62f8f69fe73ba23fe8fc866d574
patryk <45935681+patrykmrozek@users.noreply.github.com>
2025-10-23 03:13:11 +0800

    Update ScoreCommandTest.java - remove empty catch blocks

--- src/test/java/seedu/duke/ScoreCommandTest.java
+++ src/test/java/seedu/duke/ScoreCommandTest.java
@@ -1,46 +1,39 @@
 package seedu.duke;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ScoreCommandTest {
 
     @Test
     public void execute_validBreakdown_savesScoresCorrectly() throws UniflowException {
         ModuleList modules = new ModuleList();
-        modules.addModule(new Module("CS1010","Programming","Mon","10:00","12:00","Lecture"));
+        modules.addModule(new Module("CS1010", "Programming", "Mon", "10:00", "12:00", "Lecture"));
         UI ui = new UI();
+
         new ScoreCommand("CS1010", "exam:50 project:40").execute(ui, modules);
 
-        Module m = modules.getModuleByID("CS1010");
-        assertEquals(2, m.getScoreBreakdown().size());
-        assertEquals(Integer.valueOf(50), m.getScoreBreakdown().get("exam"));
-        assertEquals(Integer.valueOf(40), m.getScoreBreakdown().get("project"));
+        Module module = modules.getModuleByID("CS1010");
+        assertEquals(2, module.getScoreBreakdown().size());
+        assertEquals(Integer.valueOf(50), module.getScoreBreakdown().get("exam"));
+        assertEquals(Integer.valueOf(40), module.getScoreBreakdown().get("project"));
     }
 
-    @Test
-    public void execute_invalidValue_throwsException() {
+    @Test(expected = UniflowException.class)
+    public void execute_invalidValue_throwsException() throws UniflowException {
         ModuleList modules = new ModuleList();
-        modules.addModule(new Module("CS1234","Test","Mon","9:00","10:00","Lecture"));
+        modules.addModule(new Module("CS1234", "Test", "Mon", "9:00", "10:00", "Lecture"));
         UI ui = new UI();
 
-        try {
-            new ScoreCommand("CS1234", "exam:abc").execute(ui, modules);
-            fail("Expected UniflowException");
-        } catch (UniflowException expected) {
-        }
+        new ScoreCommand("CS1234", "exam:abc").execute(ui, modules);
     }
 
-    @Test
-    public void execute_moduleDoesNotExist_throwsException() {
+    @Test(expected = UniflowException.class)
+    public void execute_moduleDoesNotExist_throwsException() throws UniflowException {
         ModuleList modules = new ModuleList();
         UI ui = new UI();
 
-        try {
-            new ScoreCommand("CS9999", "exam:50").execute(ui, modules);
-            fail("Expected UniflowException");
-        } catch (UniflowException expected) {
-        }
+        new ScoreCommand("CS9999", "exam:50").execute(ui, modules);
     }
 }
