5efb685c491ff4cef1cf84bbf84f64719cbd7525
patryk <45935681+patrykmrozek@users.noreply.github.com>
2025-10-23 03:16:13 +0800

    Update ScoreCommandTest.java - correct JUnit imports

--- src/test/java/seedu/duke/ScoreCommandTest.java
+++ src/test/java/seedu/duke/ScoreCommandTest.java
@@ -1,8 +1,8 @@
 package seedu.duke;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ScoreCommandTest {
 
