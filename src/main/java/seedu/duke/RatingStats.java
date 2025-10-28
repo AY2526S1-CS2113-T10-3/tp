82081c124256aec9a0345223850e233da9548e22
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-10-28 23:54:01 +0800

    Update RatingStats, added constructor

--- src/main/java/seedu/duke/RatingStats.java
+++ src/main/java/seedu/duke/RatingStats.java
@@ -4,6 +4,13 @@ public final class RatingStats {
     private int sum;
     private int count;
 
+    public RatingStats() {}
+
+    public RatingStats(int sum, int count) {
+        this.sum = sum;
+        this.count = count;
+    }
+
     public void add(int score) {
         sum += score;
         count++;
