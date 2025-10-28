5ae6f9a14aead68826b4e82df3ebe01dea926271
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-10-28 23:31:32 +0800

    Add methods to RatingManager

--- src/main/java/seedu/duke/RatingManager.java
+++ src/main/java/seedu/duke/RatingManager.java
@@ -1,4 +1,27 @@
 package seedu.duke;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RatingManager {
+    private final Map<String, RatingStats> moduleRating = new HashMap<>();
+
+    public void addRating(String moduleCode, int score) {
+        moduleRating.computeIfAbsent(moduleCode.toLowerCase(), c -> new RatingStats()).add(score);
+    }
+
+    public int getCount(String moduleCode) {
+        RatingStats stat = moduleRating.get(moduleCode.toLowerCase());
+        return stat == null ? 0 : stat.getCount();
+    }
+
+    public Double getAverage(String moduleCode) {
+        RatingStats stat = moduleRating.get(moduleCode.toLowerCase());
+        return stat == null ? 0.0 : stat.average();
+    }
+
+    public Map<String, RatingStats> getModuleRating() {
+        return moduleRating;
+    }
 }
+
