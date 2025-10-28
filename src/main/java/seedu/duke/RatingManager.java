bf8f5f187570e7a5d782847b55e195ae766ea844
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-10-29 00:21:36 +0800

    Update RatingManager

--- src/main/java/seedu/duke/RatingManager.java
+++ src/main/java/seedu/duke/RatingManager.java
@@ -4,24 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RatingManager {
-    private final Map<String, RatingStats> moduleRating = new HashMap<>();
+    private RatingStorage storage = new  RatingStorage();
+    private Map<String, RatingStats> moduleRatings = new HashMap<>();
+
+    public RatingManager() {
+        this(new RatingStorage());
+    }
+
+    public RatingManager(RatingStorage storage) {
+        this.storage = storage;
+        Map<String, RatingStats> loaded = storage.load();
+        this.moduleRatings = (loaded != null) ? loaded : new HashMap<>();
+    }
 
     public void addRating(String moduleCode, int score) {
-        moduleRating.computeIfAbsent(moduleCode.toLowerCase(), c -> new RatingStats()).add(score);
+        moduleRatings.computeIfAbsent(moduleCode.toLowerCase(), c -> new RatingStats()).add(score);
+        storage.save(moduleRatings);
     }
 
     public int getCount(String moduleCode) {
-        RatingStats stat = moduleRating.get(moduleCode.toLowerCase());
+        RatingStats stat = moduleRatings.get(moduleCode.toLowerCase());
         return stat == null ? 0 : stat.getCount();
     }
 
     public Double getAverage(String moduleCode) {
-        RatingStats stat = moduleRating.get(moduleCode.toLowerCase());
+        RatingStats stat = moduleRatings.get(moduleCode.toLowerCase());
         return stat == null ? 0.0 : stat.average();
     }
 
-    public Map<String, RatingStats> getModuleRating() {
-        return moduleRating;
+    public Map<String, RatingStats> getmoduleRatings() {
+        return moduleRatings;
     }
 }
 
