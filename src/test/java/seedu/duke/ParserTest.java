5436b9471cb22c0126c944d5285bcae06f85d0b9
Leo Cheng <lefan.cheng@outlook.com>
2025-10-30 23:00:37 +0800

    Update ParserTest.java

--- src/test/java/seedu/duke/ParserTest.java
+++ src/test/java/seedu/duke/ParserTest.java
@@ -3,205 +3,147 @@ package seedu.duke;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ParserTest {
+class ParserTest {
 
     @Test
-    public void parse_byeCommand_returnsExitCommand() throws UniflowException {
+    void parse_unknownCommand() {
+        assertThrows(UniflowException.class, () -> {
+            Parser.parse("unknown command");
+        });
+    }
+
+    @Test
+    void parse_exitCommand() throws UniflowException {
         Command command = Parser.parse("bye");
         assertInstanceOf(ExitCommand.class, command);
-        assertTrue(command.isExit());
     }
 
     @Test
-    public void parse_listCommand_returnsListCommand() throws UniflowException {
-        Command command = Parser.parse("list");
-        assertInstanceOf(ListCommand.class, command);
-    }
-
-    @Test
-    public void parse_gpaCommand_returnsComputeGpaCommand() throws UniflowException {
-        Command command = Parser.parse("gpa");
-        assertInstanceOf(ComputeGpaCommand.class, command);
-    }
-
-    @Test
-    public void parse_projectGpaCommand_returnsProjectGpaCommand() throws UniflowException {
-        Command command = Parser.parse("projectgpa");
-        assertInstanceOf(ProjectGpaCommand.class, command);
-    }
-
-    @Test
-    public void parse_showTimetableCommand_returnsShowTimetableCommand() throws UniflowException {
-        Command command = Parser.parse("show timetable");
-        assertInstanceOf(ShowTimetableCommand.class, command);
-    }
-
-    @Test
-    public void parse_resetTimetableCommand_returnsResetTimetableCommand() throws UniflowException {
-        Command command = Parser.parse("reset timetable");
-        assertInstanceOf(ResetTimetableCommand.class, command);
-    }
-
-    @Test
-    public void parse_insertCommand_validFormat_returnsInsertCommand() throws UniflowException {
-        Command command = Parser.parse("insert i/CS2113 n/Software d/Monday f/14:00 t/16:00 s/lecture");
+    void parse_insertCommand() throws UniflowException {
+        Command command = Parser.parse("insert i/CS2113 n/Software Engineering d/Monday f/09:00 t/10:00 s/Tutorial");
         assertInstanceOf(InsertCommand.class, command);
     }
 
     @Test
-    public void parse_insertCommand_missingFields_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("insert i/CS2113 n/Software"));
-    }
-
-    @Test
-    public void parse_deleteCommand_validFormat_returnsDeleteCommand() throws UniflowException {
+    void parse_deleteCommand() throws UniflowException {
         Command command = Parser.parse("delete i/CS2113");
         assertInstanceOf(DeleteCommand.class, command);
     }
 
     @Test
-    public void parse_deleteCommand_invalidFormat_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("delete CS2113"));
+    void parse_listCommand() throws UniflowException {
+        Command command = Parser.parse("list");
+        assertInstanceOf(ListCommand.class, command);
     }
 
     @Test
-    public void parse_deleteCommand_missingId_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("delete i/"));
-    }
-
-    @Test
-    public void parse_filterCommand_typeFilter_returnsFilterCommand() throws UniflowException {
-        Command command = Parser.parse("filter type/lecture");
-        assertInstanceOf(FilterCommand.class, command);
-    }
-
-    @Test
-    public void parse_filterCommand_hasTutorial_returnsFilterCommand() throws UniflowException {
-        Command command = Parser.parse("filter hastutorial");
-        assertInstanceOf(FilterCommand.class, command);
-    }
-
-    @Test
-    public void parse_filterCommand_noTutorial_returnsFilterCommand() throws UniflowException {
-        Command command = Parser.parse("filter notutorial");
-        assertInstanceOf(FilterCommand.class, command);
-    }
-
-    @Test
-    public void parse_filterCommand_dayFilter_returnsFilterCommand() throws UniflowException {
+    void parse_filterCommand() throws UniflowException {
         Command command = Parser.parse("filter day/Monday");
         assertInstanceOf(FilterCommand.class, command);
     }
 
     @Test
-    public void parse_filterCommand_emptyValue_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("filter type/"));
-    }
-
-    @Test
-    public void parse_filterCommand_noParameters_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("filter"));
-    }
-
-    @Test
-    public void parse_addGradeCommand_validFormat_returnsAddGradeCommand() throws UniflowException {
-        Command command = Parser.parse("addgrade c/CS2113 cr/4 g/A m/true");
-        assertInstanceOf(AddGradeCommand.class, command);
-    }
-
-    @Test
-    public void parse_addGradeCommand_missingFields_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("addgrade c/CS2113 cr/4"));
-    }
-
-    @Test
-    public void parse_addTestGradeCommand_validFormat_returnsAddTestGradeCommand() throws UniflowException {
-        Command command = Parser.parse("addtestgrade c/CS2113 cr/4 g/A m/true");
-        assertInstanceOf(AddTestGradeCommand.class, command);
-    }
-
-    @Test
-    public void parse_scoreCommand_validFormat_returnsScoreCommand() throws UniflowException {
+    void parseScoreCommand_validFormat() throws UniflowException {
         Command command = Parser.parse("score CS2113 exam:50 project:30");
         assertInstanceOf(ScoreCommand.class, command);
     }
 
     @Test
-    public void parse_scoreCommand_queryMode_returnsScoreCommand() throws UniflowException {
+    void parseScoreCommand_queryMode() throws UniflowException {
         Command command = Parser.parse("score CS2113 -1");
         assertInstanceOf(ScoreCommand.class, command);
     }
 
     @Test
-    public void parse_scoreCommand_missingId_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("score"));
+    void parseScoreCommand_missingId() {
+        assertThrows(UniflowException.class, () -> {
+            Parser.parse("score");
+        });
     }
 
     @Test
-    public void parse_rateCommand_withRating_returnsRateCommand() throws UniflowException {
-        Command command = Parser.parse("rate CS2113 5");
+    void parse_addGradeCommand() throws UniflowException {
+        Command command = Parser.parse("addgrade c/CS2113 cr/4 g/A m/true");
+        assertInstanceOf(AddGradeCommand.class, command);
+    }
+
+    @Test
+    void parse_addTestGradeCommand() throws UniflowException {
+        Command command = Parser.parse("addtestgrade c/CS2113 cr/4 g/A m/true");
+        assertInstanceOf(AddTestGradeCommand.class, command);
+    }
+
+    @Test
+    void parse_gpaCommand() throws UniflowException {
+        Command command = Parser.parse("gpa");
+        assertInstanceOf(ComputeGpaCommand.class, command);
+    }
+
+    @Test
+    void parse_projectGpaCommand() throws UniflowException {
+        Command command = Parser.parse("projectgpa");
+        assertInstanceOf(ProjectGpaCommand.class, command);
+    }
+
+    @Test
+    void parse_showTimetableCommand() throws UniflowException {
+        Command command = Parser.parse("show timetable");
+        assertInstanceOf(ShowTimetableCommand.class, command);
+    }
+
+    @Test
+    void parse_resetTimetableCommand() throws UniflowException {
+        Command command = Parser.parse("reset timetable");
+        assertInstanceOf(ResetTimetableCommand.class, command);
+    }
+
+    @Test
+    void parseRateCommand_withRating() throws UniflowException {
+        Command command = Parser.parse("rate CS2113 4");
         assertInstanceOf(RateCommand.class, command);
     }
 
     @Test
-    public void parse_rateCommand_queryMode_returnsRateCommand() throws UniflowException {
+    void parseRateCommand_queryMode() throws UniflowException {
         Command command = Parser.parse("rate CS2113");
         assertInstanceOf(RateCommand.class, command);
     }
 
     @Test
-    public void parse_rateCommand_invalidRating_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("rate CS2113 6"));
-
-        assertThrows(UniflowException.class, () -> Parser.parse("rate CS2113 0"));
+    void parseRateCommand_invalidRating() {
+        assertThrows(UniflowException.class, () -> {
+            Parser.parse("rate CS2113 10");
+        });
     }
 
     @Test
-    public void parse_reviewCommand_validFormat_returnsReviewCommand() throws UniflowException {
+    void parseReviewCommand_validFormat() throws UniflowException {
         Command command = Parser.parse("review CS2113");
         assertInstanceOf(ReviewCommand.class, command);
     }
 
     @Test
-    public void parse_addReviewCommand_validFormat_returnsAddReviewCommand() throws UniflowException {
-        Command command = Parser.parse("addreview c/CS2113 u/John r/Great course!");
+    void parseAddReviewCommand_validFormat() throws UniflowException {
+        Command command = Parser.parse("addreview c/CS2113 u/John r/Great module!");
         assertInstanceOf(AddReviewCommand.class, command);
     }
 
     @Test
-    public void parse_addReviewCommand_missingFields_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("addreview c/CS2113 u/John"));
+    void parseAddReviewCommand_missingFields() {
+        assertThrows(UniflowException.class, () -> {
+            Parser.parse("addreview c/CS2113");
+        });
     }
 
     @Test
-    public void parse_editReviewCommand_validFormat_returnsEditReviewCommand() throws UniflowException {
+    void parseEditReviewCommand_validFormat() throws UniflowException {
         Command command = Parser.parse("editreview c/CS2113 u/John r/Updated review");
         assertInstanceOf(EditReviewCommand.class, command);
     }
 
     @Test
-    public void parse_deleteReviewCommand_validFormat_returnsDeleteReviewCommand() throws UniflowException {
+    void parseDeleteReviewCommand_validFormat() throws UniflowException {
         Command command = Parser.parse("deletereview c/CS2113 u/John");
         assertInstanceOf(DeleteReviewCommand.class, command);
-    }
-
-    @Test
-    public void parse_emptyCommand_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse(""));
-    }
-
-    @Test
-    public void parse_nullCommand_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse(null));
-    }
-
-    @Test
-    public void parse_whitespaceCommand_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("   "));
-    }
-
-    @Test
-    public void parse_unknownCommand_throwsException() {
-        assertThrows(UniflowException.class, () -> Parser.parse("unknown command"));
     }
 }
