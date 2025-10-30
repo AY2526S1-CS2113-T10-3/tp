a88777f52aed8d599e1ba02991fd63685ea3799c
Leo Cheng <lefan.cheng@outlook.com>
2025-10-30 22:48:42 +0800

    Update import lists

--- src/test/java/seedu/duke/ModuleListTest.java
+++ src/test/java/seedu/duke/ModuleListTest.java
@@ -2,7 +2,13 @@ package seedu.duke;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ModuleListTest {
     private ModuleList moduleList;
