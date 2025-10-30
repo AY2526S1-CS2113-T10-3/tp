57290c4660b736070254f7befe2e1e93dcf44db2
Leo Cheng <lefan.cheng@outlook.com>
2025-10-30 09:02:09 +0800

    Update DeleteReviewCommand.java

--- src/main/java/seedu/duke/DeleteReviewCommand.java
+++ src/main/java/seedu/duke/DeleteReviewCommand.java
@@ -1,5 +1,27 @@
 package seedu.duke;
 
-public class DeleteReviewCommand {
-    
+public class DeleteReviewCommand extends Command {
+    private final String course;
+    private final String user;
+    private final ReviewManager reviewManager;
+
+    public DeleteReviewCommand(String course, String user, ReviewManager reviewManager) {
+        this.course = course;
+        this.user = user;
+        this.reviewManager = reviewManager;
+    }
+
+    @Override
+    public void execute(UI ui, ModuleList modules, CourseRecord courseRecord) throws UniflowException {
+        if (!reviewManager.hasUserReview(course, user)) {
+            throw new UniflowException("You have not posted a review for " + course + " yet.");
+        }
+
+        boolean success = reviewManager.deleteReview(course, user);
+        if (success) {
+            ui.showMessage("Review deleted for " + course + ".");
+        } else {
+            throw new UniflowException("Failed to delete review for " + course + ".");
+        }
+    }
 }
