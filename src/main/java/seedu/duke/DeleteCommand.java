caf50385b4948e825619de3252efb73db12ed661
Philedo🦖 <seahsongli@gmail.com>
2025-11-01 13:36:52 +0800

    Deletion by index instead of course ID

--- src/main/java/seedu/duke/DeleteCommand.java
+++ src/main/java/seedu/duke/DeleteCommand.java
@@ -1,15 +1,14 @@
 package seedu.duke;
 
 public class DeleteCommand extends Command {
-    private final String moduleId;
-
-    DeleteCommand(String moduleId) {
-        this.moduleId = moduleId;
+    private final int index;
+    DeleteCommand(int index) {
+        this.index = index;
     }
 
     @Override
     public void execute(UI ui, ModuleList modules, CourseRecord courseRecord) throws UniflowException {
-        Module deletedModule = modules.deleteModuleById(moduleId);
+        Module deletedModule = modules.deleteModule(index - 1);
         ui.showMessage("Noted. I've removed this module:\n  " + deletedModule.toString() +
                 "\nNow you have " + modules.getSize() + " module(s) left in the list.");
     }
