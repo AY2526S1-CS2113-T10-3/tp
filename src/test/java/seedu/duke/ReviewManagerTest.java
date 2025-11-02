2df85ec310231319f54ddfaf10c05c7665c25510
sandrej09 <makg.pacz@gmail.com>
2025-11-02 12:17:48 +0300

    Restore list command, update Parser and fix Checkstyle issue

--- src/test/java/seedu/duke/ReviewManagerTest.java
+++ src/test/java/seedu/duke/ReviewManagerTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ReviewManagerTest {
 
