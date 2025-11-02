bde0ed5ca1c5155ea63ef3cffb99aa78ab34eb27
sandrej09 <makg.pacz@gmail.com>
2025-11-02 07:18:18 +0300

    Add ReviewStorage improvements and sample reviews file

--- src/main/java/seedu/duke/ReviewStorage.java
+++ src/main/java/seedu/duke/ReviewStorage.java
@@ -1,7 +1,15 @@
 package seedu.duke;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ReviewStorage {
     private static final String FILE_PATH = "data/reviews.txt";
