07d965e36db03f0618504e1f3a5ea626df1e79c3
Leo Cheng <lefan.cheng@outlook.com>
2025-11-04 07:24:26 +0800

    Update ReviewManagerTest.java

--- src/test/java/seedu/duke/ReviewManagerTest.java
+++ src/test/java/seedu/duke/ReviewManagerTest.java
@@ -11,7 +11,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class ReviewManagerTest {
 
